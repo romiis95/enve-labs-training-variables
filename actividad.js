16704294bec5c3eb76c5d18cded9dba915c3f618
romiis95 <romiirodriguez89@gmail.com>
2023-06-14 11:39:35 -0300

    actividad variables

---
+++ actividad.js
@@ -0,0 +1,133 @@
+//1- variables y tipos de datos
+
+let nombre = "Romina";
+let edad = "28";
+
+console.log("Mi nombre es: " + nombre);
+console.log("Mi edad es: " + edad);
+
+let esMayorDeEdad = parseInt(edad) >= 18;
+
+console.log("¿Es mayor de edad? " + esMayorDeEdad);
+
+edad = 28
+año-de-nacimiento + datetime.dato.año().año -edad
+
+console.log("tu año de nacimiento es:", año-de-nacimiento);
+
+//2- operadores arimetricos
+
+//Asigna los números a las variables
+num1 = 10
+num2 = 3
+
+//Calcula la suma
+suma = num1 + num2
+
+//Calcula la resta
+resta = num1 - num2
+
+//Calcula la multiplicación
+multiplicacion = num1 * num2
+
+//Calcula la división
+division = num1 / num2
+
+//Calcula el módulo
+modulo = num1 % num2
+
+//muestra los resultados
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("Módulo:", modulo)
+
+//Asigna los números a las variables
+num1 = 10
+num2 = 3
+
+//Incrementa en uno el valor de num1
+num1 += 1
+
+//Decrementa en uno el valor de num2
+num2 -= 1
+
+//muestra los valores actualizados
+print("num1 incrementado:", num1)
+print("num2 decrementado:", num2)
+
+//3- operadores de comparacion
+
+//Asigna los números a las variables
+num1 = 10
+num2 = 10
+
+//Asigna los números a las variables
+num1 = 10
+num2 = 5
+
+//Comprueba si num1 es mayor que num2
+if (num1 >= num2)
+print("num1 es mayor que num2")
+
+//Comprueba si num1 es menor que num2
+if (num1 < num2)
+print("num1 es menor que num2")
+
+//Comprueba si num1 es mayor o igual que num2
+if (num1 >= num2)
+print("num1 es mayor o igual que num2")
+
+//Comprueba si num1 es menor o igual que num2
+if (num1 <= num2)
+print("num1 es menor o igual que num2")
+
+
+//Asigna la edad a la variable
+edad = 15
+
+//Comprueba si la edad está dentro del rango de adolescencia
+esAdolescente = 13 <= edad <= 19
+
+//Imprime el resultado
+if (esAdolescente >=19)
+print("La persona es adolescente")
+else 
+
+print("La persona no es adolescente")
+
+
+//Asigna la edad a la variable
+    edad = 70
+    
+//Comprueba si la edad es menor que 13 o mayor que 65
+    esNinoOJubilado = edad < 13 > 65
+    
+//Imprime el resultado
+if (esNinoOJubilado)
+print("La persona es un niño o jubilado")
+else
+print("La persona no es un niño ni jubilado")
+    
+//Supongamos que la variable esMayorDeEdad tiene el valor booleano verdadero (True)
+esMayorDeEdad = True
+
+//Comprueba si esMayorDeEdad es verdadero o falso
+if (esMayorDeEdad)
+    print("Puedes votar")
+else
+    print("No puedes votar")
+
+
+    //Asigna el puntaje a la variable
+    puntaje = 75
+    
+    //Verifica el puntaje y muestra el mensaje correspondiente
+    if (puntaje < 60)
+        print("Reprobado")
+    if (puntaje < 90)
+        print("Aprobado")
+    else
+        print("Excelente")
+    
